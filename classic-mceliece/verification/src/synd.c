a97efc57f82a44dcaae0dde646d62bdd3b81b5d1
Wrenna Robson <wren.robson@gmail.com>
2021-03-17 19:18:00 +0000

    Tentative steps towards synd verification - issues with large loop overrides.

--- classic-mceliece/verification/src/synd.c
+++ classic-mceliece/verification/src/synd.c
@@ -34,3 +34,90 @@ void synd(gf *out, gf *f, gf *L, unsigned char *r)
 	}
 }
 
+void synd_loop(gf *out, gf *f, gf li, gf c)
+{
+  int j;
+	gf e, e_inv;
+  e = eval(f, li);
+  e_inv = gf_inv(gf_mul(e,e));
+
+  for (j = 0; j < 2*SYS_T; j++)
+  {
+    out[j] = gf_add(out[j], gf_mul(e_inv, c));
+    e_inv = gf_mul(e_inv, li);
+  }
+}
+
+void synd_outer(gf *out, gf *f, gf *L, unsigned char *r)
+{
+	int i;
+	gf c, li;
+
+	for (i = 0; i < 2*SYS_T; i++)
+		out[i] = 0;
+
+	for (i = 0; i < SYS_N; i++)
+	{
+		c = (r[i/8] >> (i%8)) & 1;
+    li = L[i];
+    synd_loop(out, f, li, c);
+	}
+}
+
+/*
+void synd_loop_2(gf *out, gf e, gf li)
+{
+  int j;
+	gf e_inv;
+
+  e_inv = gf_inv(gf_mul(e,e));
+  for (j = 0; j < 2*SYS_T; j++)
+  {
+    out[j] = gf_add(out[j], e_inv);
+    e_inv = gf_mul(e_inv, li);
+  }
+}
+
+void synd_outer_2(gf *out, gf *f, gf *L, unsigned char *r)
+{
+	int i;
+
+  gf rootL[SYS_N];
+
+  root(rootL, f, L);
+
+	for (i = 0; i < 2*SYS_T; i++)
+		out[i] = 0;
+
+	for (i = 0; i < SYS_N; i++)
+	  if ((r[i/8] >> (i%8)) & 1) { synd_loop_2(out, rootL[i], L[i]); }
+}
+
+void synd_loop(gf *out, gf *f, gf *L, unsigned char *r, size_t i)
+{
+  int j;
+	gf c, li, e, e_inv;
+
+  c = (r[i/8] >> (i%8)) & 1;
+  li = L[i];
+  e = eval(f, li);
+  e_inv = gf_inv(gf_mul(e,e));
+
+  for (j = 0; j < 2*SYS_T; j++)
+  {
+    out[j] = gf_add(out[j], gf_mul(e_inv, c));
+    e_inv = gf_mul(e_inv, li);
+  }
+}
+
+void synd_outer(gf *out, gf *f, gf *L, unsigned char *r)
+{
+	int i;
+
+	for (i = 0; i < 2*SYS_T; i++)
+		out[i] = 0;
+
+	for (i = 0; i < SYS_N; i++)
+    synd_loop(out, f, L, r, i);
+}
+*/
